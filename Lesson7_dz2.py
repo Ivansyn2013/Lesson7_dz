48c25fdb9bb163b675a773583ace2f5a2f103153
ivan <ivansyn2013@yandex.ru>
2021-12-05 21:30:05 +0300

    Сделано задание 7 урока

--- Lesson7_dz2.py
+++ Lesson7_dz2.py
@@ -1,29 +1,14 @@
 import json
-import re
-with open('config.json', 'r', encoding='utf-8') as f:
-    #print(''.join(f.readlines()), end='')
-    #print(f.readlines())
-    dir_lvl1 =[]
-    dir_lvl2 =[]
-    dir_lvl3 =[]
-    dir_lvl4 =[]
-    dir_lvl5 =[]
-    file_names = []
-    for el in f.readlines():
-        if el.count(' ') == 0 and el.count('.') ==0:
-            print(type(el))
-            dir_lvl1.append(el)
-        elif el.count(' ') == 3 and el.count('.') ==0:
-            dir_lvl2.append(el)
-        elif el.count(' ') == 5 and el.count('.') ==0:
-            dir_lvl3.append(el)
-        elif el.count(' ') == 8 and el.count('.') ==0:
-            dir_lvl4.append(el)
-        elif el.count('.') != 0:
-            file_names.append(el)
+import os
 
-    print(dir_lvl1)
-    print(dir_lvl2)
-    print(dir_lvl3)
-    print(dir_lvl4)
-    print(dir_lvl5)
+ROOT = os.path.dirname(__file__)
+
+with open('config.json','r', encoding = 'utf-8') as f:
+    conf = json.load(f)
+
+for dir_path in conf:
+    fulldir_path = os.path.join(ROOT, dir_path)
+    os.makedirs(fulldir_path, exist_ok = True)
+    for file_name in conf[dir_path]:
+        with open(os.path.join(fulldir_path, file_name), 'x', encoding='utf-8'):
+            pass
